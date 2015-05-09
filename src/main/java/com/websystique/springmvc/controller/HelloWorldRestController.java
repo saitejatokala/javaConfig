5749ae2d9b28c500cd9e0cc65a335515c61089a0
Tokala Sai Teja <tokala.teja@proptiger.com>
2015-05-09 21:57:37 +0530

    Worikig Java Config Spring MVC App

---
+++ src/main/java/com/websystique/springmvc/controller/HelloWorldRestController.java
@@ -0,0 +1,22 @@
+package com.websystique.springmvc.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.websystique.springmvc.domain.Message;
+ 
+@Controller
+public class HelloWorldRestController {
+ 
+    @RequestMapping("/hello/{player}")
+    @ResponseBody
+    public Message message(@PathVariable String player) {
+ 
+        Message msg = new Message(player, "Hello " + player);
+        return msg;
+    }
+ 
+}
